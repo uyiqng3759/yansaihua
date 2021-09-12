2b2da65267ec9372d577e516a925f87a87114baa
uyiqng3759 <82031326+uyiqng3759@users.noreply.github.com>
2021-09-13 07:20:59 +0800

    Update JD_ddgc.js

--- JD_ddgc.js
+++ JD_ddgc.js
@@ -44,7 +44,15 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = ['T0159KUiH11Mq1bSKBoCjVWnYaS5kRrbA', 'T0225KkcRh9P9FbRKUygl_UJcgCjVWnYaS5kRrbA', 'T011tvV3SBcQ8VwCjVWnYaS5kRrbA', 'T0225KkcR0pM91aBIhmgxf9bcACjVWnYaS5kRrbA', 'T0205KkcEV9ThDGWdWGw0K5uCjVWnYaS5kRrbA', 'T0225KkcRktN8lyBdEj1kaQMdwCjVWnYaS5kRrbA', 'P04z54XCjVWnYaS5uCHk7RxfanmDaDzc6FquQ'];
+const inviteCodes = 
+['T014-7QyGUBO9lPSKQCjVWnYaS5kRrbA',
+'T020-5J1FB5L9FWFchL9wKVeCjVWnYaS5kRrbA',
+'T0225KkcRBdI8waFdhilwKEOdwCjVWnYaS5kRrbA',
+'T0205KkcKlRwriurR0ylzbFXCjVWnYaS5kRrbA',
+'T0225KkcRRhMoFOCJkv1xqQJdgCjVWnYaS5kRrbA', 
+'T0225KkcRBYQ9VOBKBymwqECdACjVWnYaS5kRrbA', 
+'T0205KkcCnZ5tQqSfVyF7aZ0CjVWnYaS5kRrbA'
+];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {

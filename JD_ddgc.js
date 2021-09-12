fe7a22b1b854388c5485682796b5ab170bb9ca5a
uyiqng3759 <82031326+uyiqng3759@users.noreply.github.com>
2021-09-13 07:23:21 +0800

    Update JD_ddgc.js

--- JD_ddgc.js
+++ JD_ddgc.js
@@ -45,7 +45,8 @@ if ($.isNode()) {
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = 
-['T014-7QyGUBO9lPSKQCjVWnYaS5kRrbA',
+['T0205KkcA11DkzW0U0-c0oFRCjVWnYaS5kRrbA',
+'T014-7QyGUBO9lPSKQCjVWnYaS5kRrbA',
 'T020-5J1FB5L9FWFchL9wKVeCjVWnYaS5kRrbA',
 'T0225KkcRBdI8waFdhilwKEOdwCjVWnYaS5kRrbA',
 'T0205KkcKlRwriurR0ylzbFXCjVWnYaS5kRrbA',

99b64505526b8d5afa898d2a283a540395fe4698
uyiqng3759 <82031326+uyiqng3759@users.noreply.github.com>
2021-09-27 23:45:05 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -43,12 +43,10 @@ let tuanActiveId = ``, hasSend = false;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [
-  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@OXv_bToP61IiyBIjWGCC4g==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@Fan6Tj1v2hZcq9DJgaEI8Q==@EYuGfc73r8tQm37-Cup9YQ==
-',
-  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@OXv_bToP61IiyBIjWGCC4g==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@Fan6Tj1v2hZcq9DJgaEI8Q==@ EYuGfc73r8tQm37-Cup9YQ==
-',
-  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@OXv_bToP61IiyBIjWGCC4g==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@Fan6Tj1v2hZcq9DJgaEI8Q==@',
-  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@OXv_bToP61IiyBIjWGCC4g==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@Fan6Tj1v2hZcq9DJgaEI8Q==@',
+  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@EYuGfc73r8tQm37-Cup9YQ==@',
+  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@EYuGfc73r8tQm37-Cup9YQ==@',
+  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@',
+  'YbvaX1bh1SRXeSS52lv9TA==@GS-vxCkwwgVK6WvK_AS7iw==@JPXSbAmxez0nB_G_iCCbZQ==@EkwbkSQFQ-9nEwKAYM0yVA==@dJ0DhZaeGSzWcIU-zRWjdw==@',
 ];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.tuanIds = [];
